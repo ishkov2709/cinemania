0fce0e65c1fdf59757ea97dbe3885d3ba711ff5a
Dmitry Ishkov <dmitryishkov27@gmail.com>
2023-09-04 23:10:21 +0300

    fixed scroll to top

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -5,7 +5,9 @@ import { Btn } from './ScrollToTop.styled';
 const ScrollToTop = ({ handler }) => {
   return ReactDOM.createPortal(
     <Btn type="button" onClick={handler}>
-      <GiFlamingArrow size={65} style={{ transform: 'rotate(225deg)' }} />
+      <GiFlamingArrow
+        style={{ transform: 'rotate(225deg)', width: 'inherit', height: 'inherit' }}
+      />
     </Btn>,
     document.getElementById('portal-root')
   );
--- src/components/ScrollToTop/ScrollToTop.styled.jsx
+++ src/components/ScrollToTop/ScrollToTop.styled.jsx
@@ -2,8 +2,11 @@ import styled from '@emotion/styled';
 
 export const Btn = styled.button`
   position: fixed;
-  bottom: 25px;
+  bottom: 125px;
   right: 25px;
+
+  width: 85px;
+  height: 85px;
 
   background: transparent;
   color: #fff;
@@ -11,7 +14,7 @@ export const Btn = styled.button`
 
   z-index: 1000;
 
-  animation: arrowTransition 2000ms linear infinite;
+  animation: arrowMobTransition 2000ms linear infinite;
 
   transition: color 250ms ease-in-out, transform 250ms ease-in-out, bottom 250ms ease-in-out;
 
@@ -21,5 +24,20 @@ export const Btn = styled.button`
     color: #960505;
 
     animation-play-state: paused;
+  }
+
+  @media screen and (min-width: 744px) {
+    bottom: 225px;
+
+    width: 105px;
+    height: 105px;
+
+    animation: arrowTabTransition 2000ms linear infinite;
+  }
+
+  @media screen and (min-width: 1440px) {
+    bottom: 25px;
+
+    animation: arrowDesktopTransition 2000ms linear infinite;
   }
 `;
